4bc5fec7c27cf42a8080ad23c41f9d7ba188ba6c
Nirei Orange <nireiorange@gmail.com>
2014-02-13 19:57:25 +0100

    First interfaces.

---
+++ Tobacco/src/tobacco/core/components/Component.java
@@ -0,0 +1,16 @@
+package tobacco.core.components;
+
+/**
+ * This contain all of the game's data.
+ * @author nirei
+ *
+ */
+public interface Component {
+
+	/**
+	 * Returns this component's name.
+	 * @return {@link String} representing the name of the component.
+	 */
+	public String getComponentName();
+
+}
---
+++ Tobacco/src/tobacco/core/entities/Entity.java
@@ -0,0 +1,28 @@
+package tobacco.core.entities;
+
+import java.util.Map;
+
+import tobacco.core.components.Component;
+
+/**
+ * Entity for the game engine.
+ * Contains components.
+ * No logic.
+ * @author nirei
+ *
+ */
+public interface Entity {
+
+	/**
+	 * Gets the map of components.
+	 * @return Map of {@link Component} for this entity.
+	 */
+	public Map<String, Component> getComponents();
+	
+	/**
+	 * Adds a {@link Component} to the map of components.
+	 * @param _component Component to be added
+	 */
+	public void addComponent(Component _component);
+
+}
