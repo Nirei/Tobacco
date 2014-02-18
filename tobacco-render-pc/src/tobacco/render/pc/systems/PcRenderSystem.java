dbd684b2138c0f2bbebbf99c51a2f7f0ce89c0b3
bradarseniko <bradarseniko@gmail.com>
2014-02-18 23:55:45 +0100

    brad algo fumao

--- tobacco-render-pc/src/tobacco/render/pc/systems/PcRenderSystem.java
+++ tobacco-render-pc/src/tobacco/render/pc/systems/PcRenderSystem.java
@@ -1,14 +1,26 @@
 package tobacco.render.pc.systems;
 
+
+import javax.swing.JFrame;
+
+import tobacco.core.components.Component;
 import tobacco.core.entities.Entity;
 import tobacco.core.systems.EngineSystem;
 
 public class PcRenderSystem implements EngineSystem {
 
+	private JFrame frame;
+	
+	public PcRenderSystem() {
+		this.frame = new JFrame();
+	}
+
 	@Override
 	public void work(Entity entity) {
-		// TODO Auto-generated method stub
-
+		if(entity.contains(Component.DRAWABLE_C))
+		{
+			
+		}
 	}
 
 }
