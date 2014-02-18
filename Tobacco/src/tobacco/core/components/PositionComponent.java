7ca364adea731a65f6796ce1e46b944ece1bbd5e
bradarseniko <bradarseniko@gmail.com>
2014-02-18 19:59:36 +0100

    Añadi los vectores Brad

--- Tobacco/src/tobacco/core/components/PositionComponent.java
+++ Tobacco/src/tobacco/core/components/PositionComponent.java
@@ -1,10 +1,21 @@
 package tobacco.core.components;
 
+import java.util.Vector;
+
+import tobacco.core.util.Vector2D;
+
 public class PositionComponent implements Component {
 
+	private Vector2D position; 
 	@Override
 	public int getComponentType() {
 		return POSITION_C;
+	}
+	
+	public Vector2D getPosition()
+	{
+		return new Vector2D(position.getX(),position.getY());
+		
 	}
 
 }
