edad5ce88407e4ca04f9861acbc971844866459f
bradarseniko <bradarseniko@gmail.com>
2014-02-18 16:44:35 +0100

    Creado motor de renderizado para PC.

---
+++ tobacco-render-pc/src/tobacco/render/pc/systems/PcRenderSystem.java
@@ -0,0 +1,14 @@
+package tobacco.render.pc.systems;
+
+import tobacco.core.entities.Entity;
+import tobacco.core.systems.EngineSystem;
+
+public class PcRenderSystem implements EngineSystem {
+
+	@Override
+	public void work(Entity entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
