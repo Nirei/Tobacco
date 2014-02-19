0671ba5e3e806f3e4c0133f26bf3dcf0b1b88622
Nirei Orange <nireiorange@gmail.com>
2014-02-19 15:34:36 +0100

    Deleted Tobacco directory. Deleted Tobacco directory.

--- Tobacco/src/tobacco/core/components/ScaleComponent.java
+++
@@ -1,9 +0,0 @@
-package tobacco.core.components;
-
-public class ScaleComponent implements Component {
-
-	@Override
-	public int getComponentType() {
-		return SCALE_C;
-	}
-}
--- Tobacco/src/tobacco/core/loader/Loader.java
+++
@@ -1,9 +0,0 @@
-package tobacco.core.loader;
-
-import tobacco.core.entities.Entity;
-import tobacco.core.systems.MainSystem;
-
-public interface Loader {
-	public MainSystem loadMainSystem();
-	public Entity loadRootEntity();
-}
