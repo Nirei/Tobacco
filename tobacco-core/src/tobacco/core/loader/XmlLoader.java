4afd8eb6482573c5acef4131320ed137f4b8e844
Nirei <nireiorange@gmail.com>
2014-06-29 02:54:17 +0200

    + Tobacco is as of now published under GNU GPLv3! :) * Does that mean I will not make a single penny out of it? * Probably wouldn't, anyway

--- tobacco-core/src/tobacco/core/loader/XmlLoader.java
+++ tobacco-core/src/tobacco/core/loader/XmlLoader.java
@@ -1,3 +1,23 @@
+/*
+* 	Tobacco - A portable and reusable game engine written in Java.
+*	Copyright © 2014 Nirei
+*
+*	This file is part of Tobacco
+*
+*   Tobacco is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 package tobacco.core.loader;
 
 import java.net.URL;
@@ -5,6 +25,7 @@ import java.net.URL;
 import tobacco.core.components.Entity;
 import tobacco.core.systems.MainSystem;
 
+// TODO: This class
 public class XmlLoader implements Loader {
 
 	URL systemsList, entityTree;
@@ -21,7 +42,7 @@ public class XmlLoader implements Loader {
 
 	@Override
 	public Entity loadEntityTree() {
-		// TODO Apéndice de método generado automáticamente
+		// TODO: Apéndice de método generado automáticamente
 		return null;
 	}
 
